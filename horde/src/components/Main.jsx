3a414b8b464603099cd21daeff5517105e2ed409
Sebastian <sebastian.thunberg@hotmail.se>
2024-03-25 14:23:38 +0100

    layout

--- horde/src/components/Main.jsx
+++ horde/src/components/Main.jsx
@@ -1,4 +1,30 @@
+import { Layout, Menu } from "antd";
+import Sider from "antd/es/layout/Sider";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+const items = [
+  { key: 1, label: `Rules` },
+  { key: 2, label: "" },
+];
 function Main() {
-  return <></>;
+  return (
+    <>
+      <Layout>
+        <Header>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["1"]}
+            items={items}
+          ></Menu>
+        </Header>
+        <Layout>
+          <Sider></Sider>
+          <Content></Content>
+          <Sider></Sider>
+        </Layout>
+        <Footer></Footer>
+      </Layout>
+    </>
+  );
 }
 export default Main;
